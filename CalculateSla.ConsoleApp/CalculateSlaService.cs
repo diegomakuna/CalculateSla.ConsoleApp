030b183fccd7ca275bd92b57952556d896160399
Diego Oliveira <diegomakuna@gmail.com>
2021-08-20 17:52:51 -0300

    day init out of range fix

--- CalculateSla.ConsoleApp/CalculateSlaService.cs
+++ CalculateSla.ConsoleApp/CalculateSlaService.cs
@@ -19,11 +19,9 @@ namespace CalculateSla.ConsoleApp
 
             Console.WriteLine("Normal = " + startHour + " as " + endHour);
 
-            Console.WriteLine("Normal = " + OnDutyTimeStart + " as " + OnDutyTimeEnd);
+            Console.WriteLine("Plantao = " + OnDutyTimeStart + " as " + OnDutyTimeEnd);
 
             var delivery = new DateTime(date.Ticks);
-
-            Console.WriteLine("Start = " + date);
 
             Console.WriteLine("------------------------------------------------\n");
 
@@ -34,81 +32,85 @@ namespace CalculateSla.ConsoleApp
 
             delivery = CalcTime(date, new TimeSpan(1, 0, 0), totalDays, delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd, workingDays);
 
-            #region old Code
-            //var OneHour = new TimeSpan(1, 0, 0);
-
-            //var timeLeft = TimeSpan.Zero;
-            //while (timeLeft != totalDays)
-            //{
-
-            //    var endOndute = new DateTime(delivery.Year, delivery.Month, delivery.Day, OnDutyTimeStart.Hours, OnDutyTimeStart.Minutes, 0).AddHours((day.Subtract(OnDutyTimeStart) + OnDutyTimeEnd).Hours);
-            //    if(calRange(delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd))
-            //    {
-
-            //        timeLeft = timeLeft.Add(OneHour);
-            //        delivery = delivery.Add(OneHour);
-
-            //        if (timeLeft < OneHour)
-            //        {
-            //            timeLeft = timeLeft.Add(timeLeft);
-            //            delivery = delivery.Add(timeLeft);
-            //        }
-            //    }
-            //    else
-            //    {
-
-
-            //        delivery = delivery.Add(OneHour);
-            //        if (!workingDays.Contains(delivery.DayOfWeek))
-            //        {
-            //            delivery = delivery.AddDays(1);
-            //        }
-            //    }
-
-
-
-            //}
-
-            #endregion
-
+           
             Console.WriteLine("START = " + date);
             Console.WriteLine("ENTREGA = " + delivery);
-            Console.WriteLine("ENTREGA= " + delivery.Subtract(date).Hours);
+            
 
             Console.WriteLine("------------------------------------------------\n");
         }
 
-        private static bool calRange(DateTime slaStart, DateTime delivery, TimeSpan startHour, TimeSpan endHour, TimeSpan OnDutyTimeStart, TimeSpan OnDutyTimeEnd)
+        private static bool calcRange(DateTime slaStart, DateTime delivery, TimeSpan startHour, TimeSpan endHour, TimeSpan OnDutyTimeStart, TimeSpan OnDutyTimeEnd)
         {
-            var day = new TimeSpan(24, 0, 0);
-            
-            var endOndute = new DateTime(slaStart.Year, slaStart.Month, slaStart.Day, OnDutyTimeStart.Hours, OnDutyTimeStart.Minutes, 0).AddHours((day.Subtract(OnDutyTimeStart) + OnDutyTimeEnd).Hours);
-            var StartOndute = new DateTime(slaStart.Year, slaStart.Month, slaStart.Day, OnDutyTimeStart.Hours, OnDutyTimeStart.Minutes, 0);
 
-            return (delivery.TimeOfDay >= startHour && delivery.TimeOfDay < endHour) || (delivery >= StartOndute && delivery <= endOndute);
+            var rangeStartHour = RangeDateStart(delivery, startHour, endHour);
+            var rangeEndtHour = RangeDateEnd(delivery, startHour, endHour);
+
+            var rangeStartDutyHour = RangeDateStart(delivery, OnDutyTimeStart, OnDutyTimeEnd);
+            var rangeEndDutyHour = RangeDateEnd(delivery, OnDutyTimeStart, OnDutyTimeEnd);
+
+
+            return (delivery >= rangeStartHour && delivery < rangeEndtHour) || (delivery >= rangeStartDutyHour && delivery < rangeEndDutyHour);
         }
 
-        private static DateTime CalcTime(DateTime slaStart,TimeSpan time, TimeSpan totalDays, DateTime delivery, TimeSpan startHour, TimeSpan endHour, TimeSpan OnDutyTimeStart, TimeSpan OnDutyTimeEnd, List<DayOfWeek> workingDays, bool blocked = false)
+        public static DateTime RangeDateStart(DateTime currentdate, TimeSpan start, TimeSpan end)
+        {
+
+            var day = new TimeSpan(24, 0, 0);
+
+            var ch = (end - start);
+
+            var h = ch < TimeSpan.Zero ? ch + day : ch;
+
+            return currentdate.TimeOfDay > TimeSpan.FromHours(12) ? currentdate.AddHours(-(currentdate.TimeOfDay.Hours - start.Hours)) : currentdate.Add((end - currentdate.TimeOfDay)).AddHours(-h.Hours);
+
+        }
+
+        public static DateTime RangeDateEnd(DateTime currentdate, TimeSpan start, TimeSpan end)
+        {
+
+            var day = new TimeSpan(24, 0, 0);
+
+            var ch = (end - start);
+
+            var h = ch < TimeSpan.Zero ? ch + day : ch;
+
+            var s = currentdate.TimeOfDay > TimeSpan.FromHours(12) ? currentdate.AddHours(-(currentdate.TimeOfDay.Hours - start.Hours)) : currentdate.Add((end - currentdate.TimeOfDay)).AddHours(-h.Hours);
+
+            return s.AddHours(h.Hours);
+
+        }
+
+        private static DateTime CalcTime(DateTime slaStart, TimeSpan time, TimeSpan totalDays, DateTime delivery, TimeSpan startHour, TimeSpan endHour, TimeSpan OnDutyTimeStart, TimeSpan OnDutyTimeEnd, List<DayOfWeek> workingDays, bool blocked = false)
         {
             var timeLeft = TimeSpan.Zero;
             while (timeLeft < totalDays)
             {
 
-                if (calRange(slaStart, delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd))
+                if (!workingDays.Contains(RangeDateStart(delivery, startHour, endHour).DayOfWeek) && !workingDays.Contains(RangeDateStart(delivery, OnDutyTimeStart, OnDutyTimeEnd).DayOfWeek))
+                {
+                    delivery = delivery.AddDays(1);
+                    delivery = new DateTime(delivery.Year, delivery.Month, delivery.Day, startHour.Hours, endHour.Minutes, 0);
+                    continue;
+                }
+
+                if (calcRange(slaStart, delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd))
+
                 {
 
                     if (timeLeft.Add(time) >= totalDays && !blocked)
                     {
 
-                        delivery = CalcTime(slaStart , new TimeSpan(0, 1, 0), time, delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd, workingDays, true);
+                        delivery = CalcTime(slaStart, new TimeSpan(0, 1, 0), time, delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd, workingDays, true);
 
                     }
-                    else {
+                    else
+                    {
 
                         delivery = delivery.Add(time);
                     }
-                    
-                        timeLeft = timeLeft.Add(time);
+
+                    timeLeft = timeLeft.Add(time);
 
 
                 }
@@ -129,19 +131,8 @@ namespace CalculateSla.ConsoleApp
             return delivery;
         }
 
-        public static bool InclusiveBetween(this IComparable a, IComparable b, IComparable c)
-        {
-            return a.CompareTo(b) >= 0 && a.CompareTo(c) <= 0;
-        }
 
-        public static bool ExclusiveBetween(this IComparable a, IComparable b, IComparable c)
-        {
-            return a.CompareTo(b) > 0 && a.CompareTo(c) < 0;
-        }
 
-        public static bool Between(this IComparable a, IComparable b, IComparable c)
-        {
-            return a.InclusiveBetween(b, c);
-        }
+
     }
 }
