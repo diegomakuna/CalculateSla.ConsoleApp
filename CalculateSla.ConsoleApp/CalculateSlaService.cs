53af2cad47c1e3ad08f02a2530ab61e9ae1a81d0
diego <diego@DESKTOP-08OA1IQ>
2021-08-20 00:20:53 -0300

    Add project files.

---
+++ CalculateSla.ConsoleApp/CalculateSlaService.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateSla.ConsoleApp
+{
+    public static class CalculateSlaService
+    {
+        public static void Calculate(DateTime date, List<DayOfWeek> workingDays, int hours, TimeSpan startHour, TimeSpan endHour, TimeSpan OnDutyTimeStart, TimeSpan OnDutyTimeEnd, List<DateTime> holidays)
+        {
+
+            var day = new TimeSpan(24, 0, 0);
+            var workHourPerDay = endHour.Subtract(startHour);
+            var workOnDutyPerDays = OnDutyTimeEnd < OnDutyTimeStart ? ((day.Subtract(OnDutyTimeStart)) + OnDutyTimeEnd) : OnDutyTimeEnd.Subtract(OnDutyTimeStart);
+
+            var totalworkperday = workHourPerDay + workOnDutyPerDays;
+
+            var totalDays = TimeSpan.FromHours(hours);
+
+            Console.WriteLine("Normal = " + startHour + " as " + endHour);
+
+            Console.WriteLine("Normal = " + OnDutyTimeStart + " as " + OnDutyTimeEnd);
+
+            var delivery = new DateTime(date.Ticks);
+
+            Console.WriteLine("Start = " + date);
+
+            Console.WriteLine("------------------------------------------------\n");
+
+
+
+            Console.WriteLine("TOTAL de HORAS " + totalDays);
+
+
+            delivery = CalcTime(date, new TimeSpan(1, 0, 0), totalDays, delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd, workingDays);
+
+            #region old Code
+            //var OneHour = new TimeSpan(1, 0, 0);
+
+            //var timeLeft = TimeSpan.Zero;
+            //while (timeLeft != totalDays)
+            //{
+
+            //    var endOndute = new DateTime(delivery.Year, delivery.Month, delivery.Day, OnDutyTimeStart.Hours, OnDutyTimeStart.Minutes, 0).AddHours((day.Subtract(OnDutyTimeStart) + OnDutyTimeEnd).Hours);
+            //    if(calRange(delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd))
+            //    {
+
+            //        timeLeft = timeLeft.Add(OneHour);
+            //        delivery = delivery.Add(OneHour);
+
+            //        if (timeLeft < OneHour)
+            //        {
+            //            timeLeft = timeLeft.Add(timeLeft);
+            //            delivery = delivery.Add(timeLeft);
+            //        }
+            //    }
+            //    else
+            //    {
+
+
+            //        delivery = delivery.Add(OneHour);
+            //        if (!workingDays.Contains(delivery.DayOfWeek))
+            //        {
+            //            delivery = delivery.AddDays(1);
+            //        }
+            //    }
+
+
+
+            //}
+
+            #endregion
+
+            Console.WriteLine("START = " + date);
+            Console.WriteLine("ENTREGA = " + delivery);
+            Console.WriteLine("ENTREGA= " + delivery.Subtract(date).Hours);
+
+            Console.WriteLine("------------------------------------------------\n");
+        }
+
+        private static bool calRange(DateTime slaStart, DateTime delivery, TimeSpan startHour, TimeSpan endHour, TimeSpan OnDutyTimeStart, TimeSpan OnDutyTimeEnd)
+        {
+            var day = new TimeSpan(24, 0, 0);
+            
+            var endOndute = new DateTime(slaStart.Year, slaStart.Month, slaStart.Day, OnDutyTimeStart.Hours, OnDutyTimeStart.Minutes, 0).AddHours((day.Subtract(OnDutyTimeStart) + OnDutyTimeEnd).Hours);
+            var StartOndute = new DateTime(slaStart.Year, slaStart.Month, slaStart.Day, OnDutyTimeStart.Hours, OnDutyTimeStart.Minutes, 0);
+
+            return (delivery.TimeOfDay >= startHour && delivery.TimeOfDay < endHour) || (delivery >= StartOndute && delivery <= endOndute);
+        }
+
+        private static DateTime CalcTime(DateTime slaStart,TimeSpan time, TimeSpan totalDays, DateTime delivery, TimeSpan startHour, TimeSpan endHour, TimeSpan OnDutyTimeStart, TimeSpan OnDutyTimeEnd, List<DayOfWeek> workingDays, bool blocked = false)
+        {
+            var timeLeft = TimeSpan.Zero;
+            while (timeLeft < totalDays)
+            {
+
+                if (calRange(slaStart, delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd))
+                {
+
+                    if (timeLeft.Add(time) >= totalDays && !blocked)
+                    {
+
+                        delivery = CalcTime(slaStart , new TimeSpan(0, 1, 0), time, delivery, startHour, endHour, OnDutyTimeStart, OnDutyTimeEnd, workingDays, true);
+
+                    }
+                    else {
+
+                        delivery = delivery.Add(time);
+                    }
+                    
+                        timeLeft = timeLeft.Add(time);
+
+
+                }
+                else
+                {
+
+                    delivery = delivery.Add(new TimeSpan(1, 0, 0));
+                    if (!workingDays.Contains(delivery.DayOfWeek))
+                    {
+                        delivery = delivery.AddDays(1);
+                    }
+                }
+
+
+
+            }
+
+            return delivery;
+        }
+
+        public static bool InclusiveBetween(this IComparable a, IComparable b, IComparable c)
+        {
+            return a.CompareTo(b) >= 0 && a.CompareTo(c) <= 0;
+        }
+
+        public static bool ExclusiveBetween(this IComparable a, IComparable b, IComparable c)
+        {
+            return a.CompareTo(b) > 0 && a.CompareTo(c) < 0;
+        }
+
+        public static bool Between(this IComparable a, IComparable b, IComparable c)
+        {
+            return a.InclusiveBetween(b, c);
+        }
+    }
+}
---
+++ CalculateSla.ConsoleApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculateSla.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<DayOfWeek> workingDays = new List<DayOfWeek>();
+            //workingDays.Add(DayOfWeek.Monday);
+            //workingDays.Add(DayOfWeek.Tuesday);
+            //workingDays.Add(DayOfWeek.Wednesday);
+            //workingDays.Add(DayOfWeek.Thursday);
+            //workingDays.Add(DayOfWeek.Friday);
+
+
+            //var SlaStartAt = new DateTime(2021, 08, 13, 16, 50, 0);
+            //var hours = 1;
+            //var WorkingTimeStart = new TimeSpan(7, 0, 0);
+            //var WorkingTimeEnd = new TimeSpan(17, 0, 0);
+
+            //var OnDutyTimeStart = new TimeSpan(19, 0, 0);
+            //var OnDutyTimeEnd = new TimeSpan(2, 0, 0);
+            //CalculateSlaService.Calculate(SlaStartAt, workingDays, hours, WorkingTimeStart, WorkingTimeEnd, OnDutyTimeStart, OnDutyTimeEnd, null);
+
+            //Console.WriteLine("---------------------------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------------------------");
+
+
+            //List<DayOfWeek> AworkingDays = new List<DayOfWeek>();
+            //AworkingDays.Add(DayOfWeek.Monday);
+            //AworkingDays.Add(DayOfWeek.Tuesday);
+            //AworkingDays.Add(DayOfWeek.Wednesday);
+            //AworkingDays.Add(DayOfWeek.Thursday);
+            //AworkingDays.Add(DayOfWeek.Friday);
+
+            //var ASlaStartAt = new DateTime(2021, 08, 13, 16, 50, 0);
+            //var Ahours = 10;
+            //var AWorkingTimeStart = new TimeSpan(7, 0, 0);
+            //var AWorkingTimeEnd = new TimeSpan(17, 0, 0);
+
+            //var AOnDutyTimeStart = new TimeSpan(19, 0, 0);
+            //var AOnDutyTimeEnd = new TimeSpan(2, 0, 0);
+            //CalculateSlaService.Calculate(ASlaStartAt, AworkingDays, Ahours, AWorkingTimeStart, AWorkingTimeEnd, AOnDutyTimeStart, AOnDutyTimeEnd, null);
+
+            //Console.WriteLine("---------------------------------------------------------------------------");
+
+
+            //Console.WriteLine("---------------------------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------------------------");
+
+
+            List<DayOfWeek> BworkingDays = new List<DayOfWeek>();
+            BworkingDays.Add(DayOfWeek.Monday);
+            //BworkingDays.Add(DayOfWeek.Tuesday);
+           // BworkingDays.Add(DayOfWeek.Wednesday);
+            BworkingDays.Add(DayOfWeek.Thursday);
+            BworkingDays.Add(DayOfWeek.Friday);
+            BworkingDays.Add(DayOfWeek.Saturday);
+            BworkingDays.Add(DayOfWeek.Sunday);
+
+            var BSlaStartAt = new DateTime(2021, 08, 9, 12, 59, 0);
+            var Bhours = 19;
+            var BWorkingTimeStart = new TimeSpan(7, 0, 0);
+            var BWorkingTimeEnd = new TimeSpan(17, 0, 0);
+
+            var BOnDutyTimeStart = new TimeSpan(20, 0, 0);
+            var BOnDutyTimeEnd = new TimeSpan(0, 0, 0);
+            CalculateSlaService.Calculate(BSlaStartAt, BworkingDays, Bhours, BWorkingTimeStart, BWorkingTimeEnd, BOnDutyTimeStart, BOnDutyTimeEnd, null);
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+        }
+    }
+}
